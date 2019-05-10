f09794360904f6f2db29a78d5e2ea4930f0e9dcd
Tyler Jaquish <tjaquish@stepstoneglobal.com>
2019-05-10 09:15:37 -0700

    Updated js format and removed google map.

--- js/script.js
+++ js/script.js
@@ -32,42 +32,39 @@ jQuery(function ($) { "use strict";
 	/* ========================================================================= */
 	/*	Menu item highlighting
 	/* ========================================================================= */
-
-
 	$("#navigation").sticky({
 		topSpacing : 0
 	});
-	
+		
 
 	/* ========================================================================= */
 	/*	Magnific popup
 	/* =========================================================================  */
 	$('.image-popup').magnificPopup({
-    type: 'image',
-    removalDelay: 160, //delay removal by X to allow out-animation
-    callbacks: {
-        beforeOpen: function () {
-            // just a hack that adds mfp-anim class to markup
-            this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
-            this.st.mainClass = this.st.el.attr('data-effect');
-        }
-    },
-    closeOnContentClick: true,
-    midClick: true,
-    fixedContentPos: false,
-    fixedBgPos: true
-});
+	    type: 'image',
+	    removalDelay: 160, //delay removal by X to allow out-animation
+	    callbacks: {
+	        beforeOpen: function () {
+	            // just a hack that adds mfp-anim class to markup
+	            this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+	            this.st.mainClass = this.st.el.attr('data-effect');
+	        }
+	    },
+	    closeOnContentClick: true,
+	    midClick: true,
+	    fixedContentPos: false,
+	    fixedBgPos: true
+	});
+
 	/* ========================================================================= */
 	/*	Portfolio Filtering Hook
 	/* =========================================================================  */
-
   	var mixer = mixitup('.portfolio-items-wrapper');
 	
 	/* ========================================================================= */
 	/*	Testimonial Carousel
 	/* =========================================================================  */
- 
-	//Init the carousel
+	// Init the carousel
 	$("#testimonials").slick({
 		infinite: true,
 		arrows:false,
@@ -75,15 +72,9 @@ jQuery(function ($) { "use strict";
   		autoplaySpeed: 4000
 	});
 
-
-
-
-
 	/* ========================================================================= */
 	/*   Contact Form Validating
 	/* ========================================================================= */
-
-
 	$('#contact-submit').click(function (e) {
 
 		//stop the form from being submitted
@@ -161,147 +152,23 @@ jQuery(function ($) { "use strict";
 			});
 		}
 	});
-
 });
-// End Jquery Function
 
 
-	/* ========================================================================= */
-	/*	Animated section
-	/* ========================================================================= */
+/* ========================================================================= */
+/*	Animated section
+/* ========================================================================= */
 
-	var wow = new WOW(
-		{
-		  offset:       100,          // distance to the element when triggering the animation (default is 0)
-		  mobile:       false      // trigger animations on mobile devices (default is true)
-		}
-	 );
-	 wow.init();
-
-
-	/* ========================================================================= */
-	/*	Smooth Scroll
-	/* ========================================================================= */
-	var scroll = new SmoothScroll('a[href*="#"]');
-
-
-
-	/* ========================================================================= */
-	/*	Google Map Customization
-	/* =========================================================================  */
-
-	function initialize() {
-
-		var myLatLng = new google.maps.LatLng(22.333851, 91.812256);
-
-		var roadAtlasStyles = [{
-			"featureType": "landscape",
-			"elementType": "geometry.fill",
-			"stylers": [{
-				"color": "#2F3238"
-			}]
-		}, {
-			"elementType": "labels.text.fill",
-			"stylers": [{
-				"color": "#FFFFFF"
-			}]
-		}, {
-			"elementType": "labels.text.stroke",
-			"stylers": [{
-				"visibility": "off"
-			}]
-		}, {
-			"featureType": "road",
-			"elementType": "geometry.fill",
-			"stylers": [{
-				"color": "#50525f"
-			}]
-		}, {
-			"featureType": "road",
-			"elementType": "geometry.stroke",
-			"stylers": [{
-				"visibility": "on"
-			}, {
-				"color": "#808080"
-			}]
-		}, {
-			"featureType": "poi",
-			"elementType": "labels",
-			"stylers": [{
-				"visibility": "off"
-			}]
-		}, {
-			"featureType": "transit",
-			"elementType": "labels.icon",
-			"stylers": [{
-				"visibility": "off"
-			}]
-		}, {
-			"featureType": "poi",
-			"elementType": "geometry",
-			"stylers": [{
-				"color": "#808080"
-			}]
-		}, {
-			"featureType": "water",
-			"elementType": "geometry.fill",
-			"stylers": [{
-				"color": "#3071a7"
-			}, {
-				"saturation": -65
-			}]
-		}, {
-			"featureType": "road",
-			"elementType": "labels.icon",
-			"stylers": [{
-				"visibility": "off"
-			}]
-		}, {
-			"featureType": "landscape",
-			"elementType": "geometry.stroke",
-			"stylers": [{
-				"color": "#bbbbbb"
-			}]
-		}];
-
-		var mapOptions = {
-			zoom: 14,
-			center: myLatLng,
-			disableDefaultUI: true,
-			scrollwheel: false,
-			navigationControl: false,
-			mapTypeControl: false,
-			scaleControl: false,
-			draggable: false,
-			mapTypeControlOptions: {
-				mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'roadatlas']
-			}
-		};
-
-		var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-
-		var marker = new google.maps.Marker({
-			position: myLatLng,
-			map: map,
-			title: '',
-		});
-
-
-		google.maps.event.addListener(marker, 'click', function () {
-			infowindow.open(map, marker);
-		});
-
-		var styledMapOptions = {
-			name: 'US Road Atlas'
-		};
-
-		var usRoadMapType = new google.maps.StyledMapType(
-			roadAtlasStyles, styledMapOptions);
-
-		map.mapTypes.set('roadatlas', usRoadMapType);
-		map.setMapTypeId('roadatlas');
+var wow = new WOW(
+	{
+	  offset:       100,          // distance to the element when triggering the animation (default is 0)
+	  mobile:       false      // trigger animations on mobile devices (default is true)
 	}
+ );
+ wow.init();
 
-	google.maps.event.addDomListener(window, "load", initialize);
 
+/* ========================================================================= */
+/*	Smooth Scroll
+/* ========================================================================= */
+var scroll = new SmoothScroll('a[href*="#"]');
