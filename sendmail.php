1a75612a33d9747af998b6143bd9b954f7a509aa
Tyler Jaquish <tjaquish@stepstoneglobal.com>
2019-10-30 08:41:02 -0700

    Finished implementing google recaptcha.

--- sendmail.php
+++ sendmail.php
@@ -2,43 +2,51 @@
 
 	header('Content-type: application/json');
 
-    $name = @trim(stripslashes($_POST['name'])); 
-    $email = @trim(stripslashes($_POST['email'])); 
-    $subject = @trim(stripslashes($_POST['subject'])); 
-    $message = @trim(stripslashes($_POST['message'])); 
+    $name = @trim(stripslashes($_POST['name']));
+    $email = @trim(stripslashes($_POST['email']));
+    $subject = @trim(stripslashes($_POST['subject']));
+    $message = @trim(stripslashes($_POST['message']));
+    $captcha = $_POST['captcha'];
 
-    $email_template = 'emailTemplate.html';
-    
-    $to = 'tylerjaquish@gmail.com';
+    $curlSession = curl_init();
+    curl_setopt($curlSession, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify?secret=6Le2KsAUAAAAAP5_7zUsRGo_Yjkb5scrD9Uabpn6&response={$captcha}");
+    curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
+    curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
 
-    var_dump($_POST);die;
+    $response = json_decode(curl_exec($curlSession));
+    curl_close($curlSession);
 
-    if (isset($_POST) && $message != '') {
+    if ($response->success) {
 
-        // $to = 'info@kimdoescommerce.com';
+        $emailTemplate = 'emailTemplate.html';
 
-        $headers  = "From: " . $name . ' <' . $email . '>' . "\r\n";
-        $headers .= "Reply-To: ". $email . "\r\n";
-        $headers .= "MIME-Version: 1.0\r\n";
-        $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+        if (isset($_POST) && $message != '') {
 
-        $templateTags =  array(
-            '{{subject}}' => $subject,
-            '{{email}}'=>$email,
-            '{{message}}'=>$message,
-            '{{name}}'=>$name
-        );
+            $to = 'info@kimdoescommerce.com';
 
-        $templateContents = file_get_contents( dirname(__FILE__) . '/'.$email_template);
-        $contents =  strtr($templateContents, $templateTags);
+            $headers  = "From: " . $name . ' <' . $email . '>' . "\r\n";
+            $headers .= "Reply-To: ". $email . "\r\n";
+            $headers .= "MIME-Version: 1.0\r\n";
+            $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
-        if (mail( $to, $subject, $contents, $headers)) {
-            $result = array('type' => 'success', 'message'=>'<strong>Thank You!</strong>&nbsp; Your email has been delivered.');
-        } else {
-            $result = array('type' => 'error', 'message'=>'<strong>Error!</strong>&nbsp; Can\'t Send Mail.');
+            $templateTags =  array(
+                '{{subject}}' => $subject,
+                '{{email}}'=>$email,
+                '{{message}}'=>$message,
+                '{{name}}'=>$name
+            );
+
+            $templateContents = file_get_contents( dirname(__FILE__) . '/'.$emailTemplate);
+            $contents =  strtr($templateContents, $templateTags);
+
+            if (mail( $to, $subject, $contents, $headers)) {
+                $result = array('type' => 'success', 'message'=>'<strong>Thank You!</strong>&nbsp; Your email has been delivered.');
+            } else {
+                $result = array('type' => 'error', 'message'=>'<strong>Error!</strong>&nbsp; Can\'t Send Mail.');
+            }
+
+            echo json_encode($result);
+            die;
         }
-
-        echo json_encode($result);
-        die;
     }
 ?>
